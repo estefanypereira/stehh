55020e8ba600908c679a02e354d4a6f048dc1cec
estefanypereira <142987283+estefanypereira@users.noreply.github.com>
2024-10-21 07:16:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Pizza',
+    'Qual pizza é melhor?',
+    'frango com catupyri'
+)
+
+criaCartao(
+    'Pizza',
+    'Qual é a pizza mais vendida do mundo?',
+    'A Margherita'
+)
+
+criaCartao(
+    'Pizza',
+    'Qual é a pizza tradicional?',
+    'Pizza napolitana '
+)
+
+criaCartao(
+    'Pizza',
+    'Qual País inventou a pizza?',
+    'A Itália'
+)
